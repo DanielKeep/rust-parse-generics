f455f27cb3394bef37c6e1f408e52811dfc349c6
Daniel Keep <daniel.keep@gmail.com>
2016-02-10 16:56:18 +1100

    Expanded derive_clone to cover structs.

--- parse-macros/examples/derive_clone.rs
+++ parse-macros/examples/derive_clone.rs
@@ -51,6 +51,121 @@ macro_rules! Clone_mac {
     };
 
     (
+        @item
+        struct {
+            attrs: $_attrs:tt,
+            vis: $_vis:tt,
+            name: $name:ident,
+            generics: {
+                constr: [$($constr:tt)*],
+                params: [$($params:tt)*],
+                ltimes: $_ltimes:tt,
+                tnames: [$($tnames:ident,)*],
+            },
+            where: {
+                preds: [$($preds:tt)*],
+            },
+            kind: unitary,
+            fields: [],
+            $($_struct_tail:tt)*
+        }
+    ) => {
+        Clone_mac! {
+            @inject_where
+            (impl<$($constr)*> Clone for $name<$($params)*>),
+            where ($($tnames: Clone,)* $($preds)*)
+            ({
+                fn clone(&self) -> Self {
+                    $name
+                }
+            })
+        }
+    };
+
+    (
+        @item
+        struct {
+            attrs: $_attrs:tt,
+            vis: $_vis:tt,
+            name: $name:ident,
+            generics: {
+                constr: [$($constr:tt)*],
+                params: [$($params:tt)*],
+                ltimes: $_ltimes:tt,
+                tnames: [$($tnames:ident,)*],
+            },
+            where: {
+                preds: [$($preds:tt)*],
+            },
+            kind: tuple,
+            fields: [$(
+                {
+                    ord: ($_ford:tt, $ford_ident:ident),
+                    attrs: $_fattrs:tt,
+                    vis: $_fvis:tt,
+                    ty: $_fty:ty,
+                },
+            )*],
+            $($_struct_tail:tt)*
+        }
+    ) => {
+        Clone_mac! {
+            @inject_where
+            (impl<$($constr)*> Clone for $name<$($params)*>),
+            where ($($tnames: Clone,)* $($preds)*)
+            ({
+                fn clone(&self) -> Self {
+                    match *self {
+                        $name($(ref $ford_ident),*) => $name($(Clone::clone($ford_ident)),*)
+                    }
+                }
+            })
+        }
+    };
+
+    (
+        @item
+        struct {
+            attrs: $_attrs:tt,
+            vis: $_vis:tt,
+            name: $name:ident,
+            generics: {
+                constr: [$($constr:tt)*],
+                params: [$($params:tt)*],
+                ltimes: $_ltimes:tt,
+                tnames: [$($tnames:ident,)*],
+            },
+            where: {
+                preds: [$($preds:tt)*],
+            },
+            kind: record,
+            fields: [$(
+                {
+                    ord: $_ford:tt,
+                    attrs: $_fattrs:tt,
+                    vis: $_fvis:tt,
+                    name: $fname:ident,
+                    ty: $_fty:ty,
+                },
+            )*],
+            $($_struct_tail:tt)*
+        }
+    ) => {
+        Clone_mac! {
+            @inject_where
+            (impl<$($constr)*> Clone for $name<$($params)*>),
+            where ($($tnames: Clone,)* $($preds)*)
+            ({
+                fn clone(&self) -> Self {
+                    match *self {
+                        $name { $(ref $fname),* } => $name { $($fname: Clone::clone($fname)),* }
+                    }
+                }
+            })
+        }
+    };
+
+    (
         @var_match_pat
         $name:ident,
         {
@@ -206,28 +321,68 @@ macro_rules! Clone_mac {
 }
 
 custom_derive! {
-    #[derive(Copy, Clone_mac)]
+    #[derive(Clone_mac)]
     enum EnumA {}
 }
 
 custom_derive! {
-    #[derive(Copy, Clone_mac)]
+    #[derive(Clone_mac)]
     enum EnumB { A }
 }
 
 custom_derive! {
-    #[derive(Copy, Clone_mac)]
+    #[derive(Clone_mac)]
     enum EnumC { A, B, C }
 }
 
 custom_derive! {
-    #[derive(Copy, Clone_mac)]
+    #[derive(Clone_mac)]
     enum EnumD { A, B(i32), C(u8, u8, u8) }
 }
 
 custom_derive! {
-    #[derive(Copy, Clone_mac)]
+    #[derive(Clone_mac)]
     enum EnumE { A { r: u8, g: u8, b: u8, } }
+}
+
+custom_derive! {
+    #[derive(Clone_mac)]
+    enum EnumF<T> { A { r: T, g: T, b: T, } }
+}
+
+custom_derive! {
+    #[derive(Clone_mac)]
+    struct StructA;
+}
+
+custom_derive! {
+    #[derive(Clone_mac)]
+    struct StructB(i32);
+}
+
+custom_derive! {
+    #[derive(Clone_mac)]
+    struct StructC(i32, u8, String);
+}
+
+custom_derive! {
+    #[derive(Clone_mac)]
+    struct StructD {
+        /// The red stuff.
+        r: u8,
+        pub g: u8,
+        b: u8,
+    }
+}
+
+custom_derive! {
+    #[derive(Copy, Clone_mac)]
+    struct StructE<T> {
+        /// The red stuff.
+        r: T,
+        pub g: T,
+        b: T,
+    }
 }
 
 fn main() {
@@ -240,4 +395,9 @@ fn main() {
     let _ = EnumD::B(42).clone();
     let _ = EnumD::C(1, 2, 3).clone();
     let _ = (EnumE::A { r: 1, g: 2, b: 3 }).clone();
+    let _ = (EnumF::A { r: 1, g: 2, b: 3 }).clone();
+    let _ = StructA.clone();
+    let _ = StructB(42).clone();
+    let _ = StructC(42, 2, String::from("hi!")).clone();
+    let _ = (StructD { r: 1, g: 2, b: 3 }).clone();
 }
