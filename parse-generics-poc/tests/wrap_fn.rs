b9514f0df1548300a642444387b09bf1b806650b
Daniel Keep <daniel.keep@gmail.com>
2016-04-30 17:35:28 +1000

    Added license blocks.

--- parse-generics-poc/tests/wrap_fn.rs
+++ parse-generics-poc/tests/wrap_fn.rs
@@ -1,4 +1,13 @@
-/*wrap_fn*/#![feature(plugin)]
+/*
+Copyright ⓒ 2016 rust-custom-derive contributors.
+
+Licensed under the MIT license (see LICENSE or <http://opensource.org
+/licenses/MIT>) or the Apache License, Version 2.0 (see LICENSE of
+<http://www.apache.org/licenses/LICENSE-2.0>), at your option. All
+files in the project carrying such notice may not be copied, modified,
+or distributed except according to those terms.
+*/
+#![feature(plugin)]
 #![plugin(parse_generics_poc)]
 
 use std::fmt::Display;
